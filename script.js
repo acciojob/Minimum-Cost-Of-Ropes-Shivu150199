57a97fbdeb9dc41147af9c0403243609ec955ff6
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2023-09-20 14:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function calculateMinCost() {
+function calculateMinCost(arr) {
   //your code here
+arr.sort((a,b)=>a-b)
+
+	let result=arr.reduce((a,b)=>{
+		return a+b;
+},a+b)
   
-  
-  
+  console.log(result)
 }  
